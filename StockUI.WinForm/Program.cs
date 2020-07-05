69db5ba16883c55d6787d62e033712558031325f
IT <IT@DESKTOP-NR2DND1>
2020-07-05 17:46:54 +0200

    am ready to work with stock

--- StockUI.WinForm/Program.cs
+++ StockUI.WinForm/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unity;
 
 namespace StockUI.WinForm
 {
@@ -12,11 +14,58 @@ namespace StockUI.WinForm
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+       
         static void Main()
         {
+            var container = BuildContainer();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        public static IUnityContainer BuildContainer()
+        {
+            var currentContainer = new UnityContainer();
+            //currentContainer.RegisterType<FrmTreasury>();
+            //currentContainer.RegisterType<FrmAccountHolder>();
+            //currentContainer.RegisterType<Form1>();
+            //currentContainer.RegisterType<FrmRecite>();
+            //currentContainer.RegisterType<FrmCrResite>();
+            //currentContainer.RegisterType<FrmReport>();
+            //currentContainer.RegisterType<FrmDataGrid>();
+            //currentContainer.RegisterSingleton<ISqlDataAccess, SqlDataAccess>();
+            //currentContainer.RegisterType<IAccountHolderEndPoint, AccountHolderEndPoint>();
+            //currentContainer.RegisterType<ItreasuryDetailEndPoint, treasuryDetailEndPoint>();
+            //currentContainer.RegisterType<IAccountBankTreasuryEndPoint, AccountBankTreasuryEndPoint>();
+            //currentContainer.RegisterType<ITreasuryEndPoint, TreasuryEndPoint>();
+            //currentContainer.RegisterType<IReciteEndPoint, ReciteEndPoint>();
+            //currentContainer.RegisterType<ICrReciteEndPoint, CrReciteEndPoint>();
+            //currentContainer.RegisterType<IBankEndPoint, BankEndPoint>();
+            //currentContainer.RegisterType<IDashPortEndPoint, DashPortEndPoint>();
+            //currentContainer.RegisterType<IBankTreasuryEndPoint, BankTreasuryEndPoint>();
+            //currentContainer.RegisterType<ReciteReport>();
+            currentContainer.RegisterInstance(ConfigurAtuoMaper());
+            // note: registering types could be moved off to app config if you want as well
+            return currentContainer;
+        }
+        private static IMapper ConfigurAtuoMaper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                //cfg.CreateMap<AccountHolder, AccountHolderDisplay>();
+                //cfg.CreateMap<TreasuryDetail, TreasuryDetailDisplay>();
+                //cfg.CreateMap<AccountBankTreasury, AccountBankTreasuryDisplay>();
+                //cfg.CreateMap<Treasurey, TreasureyDisplay>();
+                //cfg.CreateMap<Recite, ReciteDisplay>();
+                //cfg.CreateMap<CrResite, CrResiteDisplay>();
+                //cfg.CreateMap<Bank, BankDisplay>();
+                //cfg.CreateMap<AccountBankTreasuryDashBoard, AccountBankTreasury>().ReverseMap();
+                //cfg.CreateMap<BankTreasury, BankTreasuryDisplay>();
+                //cfg.CreateMap<AccountBankTreasuryDashBoard, AccountBankTreasury>();
+                //    cfg.CreateMap<AccountBankTreasuryDashBoard, AccountBankTreasuryDisplay>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
         }
     }
 }
