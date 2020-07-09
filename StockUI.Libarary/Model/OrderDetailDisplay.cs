bc90d1e739db436945d3e409ff20095a5f14a783
IT <IT@DESKTOP-NR2DND1>
2020-07-09 16:43:08 +0200

    order forms design

--- StockUI.Libarary/Model/OrderDetailDisplay.cs
+++ StockUI.Libarary/Model/OrderDetailDisplay.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using StockSystem.Libarary.Interfaces;
+using System;
 
 namespace StockUI.Libarary.Model
 {
-   public class OrderDetailDisplay
+    public class OrderDetailDisplay
     {
+        private readonly IItemEndPoint itemEndPoint;
+        private readonly IUnitEndPoint unitEndPoint;
+
+        public OrderDetailDisplay()
+        {
+
+        }
+        public OrderDetailDisplay(IItemEndPoint itemEndPoint,IUnitEndPoint unitEndPoint)
+        {
+            this.itemEndPoint = itemEndPoint;
+            this.unitEndPoint = unitEndPoint;
+        }
         public int Id { get; set; }
         public int orderid { get; set; }
         public int ItemId { get; set; }
@@ -16,5 +27,26 @@ namespace StockUI.Libarary.Model
         public bool FirstApprove { get; set; }
         public bool SecondApprove { get; set; }
         public bool ThirdApprove { get; set; }
+
+        public decimal  LastPrice
+        {
+            get { return 0; }
+        }
+        public string ItemName
+        {
+            get
+            {
+                var x = itemEndPoint.GetByID(ItemId);
+                return x.Name;
+            }
+        }
+        public string UnitName
+        {
+            get
+            {
+                var x = unitEndPoint.GetByID(UnitId);
+                return x.Name;
+            }
+        }
     }
 }
